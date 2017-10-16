f336c96d15c6d4426586cc709bd24fd453899cf0
Victor Starzynski <vnogstar@gmail.com>
2017-10-16 14:00:00 -0700

    First Commit

---
+++ app/src/main/java/com/example/vnstarzynski/movingtonewwest/BikeRoutesActivity.java
@@ -0,0 +1,13 @@
+package com.example.vnstarzynski.movingtonewwest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BikeRoutesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bike_routes);
+    }
+}
---
+++ app/src/main/java/com/example/vnstarzynski/movingtonewwest/EducationActivity.java
@@ -0,0 +1,16 @@
+package com.example.vnstarzynski.movingtonewwest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
